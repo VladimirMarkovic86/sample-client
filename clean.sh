41987e3bbc8cc3ebaac7088ee6c6617d9e5cadfb
Markovic Vladimir <markovic.vladimir86@gmail.com>
2018-09-13 18:20:45 +0200

    Removing unused files.

--- clean.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-lein clean
-rm -r resources/public/js/ resources/public/jsfig/ resources/public/jsprod/
--- repl.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-#	lein <command-named-figwheel> <cljsbuild-configuration>
-lein figwheel figwheel
