c36f731eec2680c31476f2a2ace454c249c0453c
VladimirMarkovic86 <markovic.vladimir86@gmail.com>
2018-10-17 11:40:56 +0200

    Heroku

--- src/clj/sample_client/core.clj
+++ src/clj/sample_client/core.clj
@@ -3,7 +3,8 @@
   (:require [ajax-lib.http.status-code :as stc]
             [ajax-lib.http.mime-type :as mt]
             [ajax-lib.http.entity-header :as eh]
-            [server-lib.core :as srvr]))
+            [server-lib.core :as srvr]
+            [environ.core :refer [env]]))
 
 (defn routing-not-found
   "Default routing if file is not found"
@@ -19,7 +20,8 @@
     (srvr/start-server
       routing-not-found
       nil
-      5000
+      (or (env :port)
+          1613)
       #_{:keystore-file-path
         "certificate/sample_client.jks"
        :keystore-password
