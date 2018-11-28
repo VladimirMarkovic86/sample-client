3ad6ccfa5cfb43ffc777871dc2b27ee5cb5dbe02
VladimirMarkovic86 <markovic.vladimir86@gmail.com>
2018-11-28 14:30:27 +0100

    Task number: 3 Refactored md5 checksum generation to work only with first 1024 bytes or less if file is smaller then 1024 bytes.

--- project.clj
+++ project.clj
@@ -15,7 +15,7 @@
                  [org.clojars.vladimirmarkovic86/sample-middle "0.3.2"]
                  [org.clojars.vladimirmarkovic86/common-middle "0.2.2"]
                  [org.clojars.vladimirmarkovic86/common-client "0.3.5"]
-                 [org.clojars.vladimirmarkovic86/server-lib "0.3.11"]
+                 [org.clojars.vladimirmarkovic86/server-lib "0.3.12"]
                  ]
 
   :min-lein-version "2.0.0"
