3d34fbcca59329270de5486aec2c08d28a842e9b
VladimirMarkovic86 <markovic.vladimir86@gmail.com>
2019-06-19 11:52:29 +0200

    Task 70: Created row and col css file and used them on home page.

--- src/cljs/sample_client/html.cljs
+++ src/cljs/sample_client/html.cljs
@@ -1,5 +1,5 @@
 (ns sample-client.html
-  (:require [htmlcss-lib.core :refer [h2 p]]
+  (:require [htmlcss-lib.core :refer [h2 p div]]
             [sample-middle.functionalities :as smfns]
             [common-client.allowed-actions.controller :refer [allowed-actions]]
             [sample-client.person.html :as ph]
@@ -10,11 +10,29 @@
 (defn home-page-content
   "Home page content"
   []
-  [(h2
-     (get-label 62))
-   (p
-     (get-label 63))]
- )
+  [(div
+     [(h2
+        (get-label 62))
+      (p
+        (get-label 63))
+      ]
+     {:class "row-1-4"})
+   (div
+     [(div
+        nil
+        {:class "col-1-4"})
+      (div
+        nil
+        {:class "col-2-4 logo-hi-res"})
+      (div
+        nil
+        {:class "col-1-4"})
+      ]
+     {:class "row-2-4"})
+   (div
+     nil
+     {:class "row-1-4"})
+   ])
 
 (defn custom-menu
   "Render menu items for user that have privilege for them"
